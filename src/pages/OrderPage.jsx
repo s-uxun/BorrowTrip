e057b8470b32790db0e24a824c09fcdbe8f63b72
s-uxun <serenasol@ewha.ac.kr>
2024-08-23 22:54:05 +0900

    Docs: App.js 및 pages들 기본 세팅 추가

---
+++ src/pages/OrderPage.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const OrderPage = () => {
+};
+
+export default OrderPage;
