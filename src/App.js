200e5d99a59e00b72db84d9e5e68b6dc707118ed
s-uxun <serenasol@ewha.ac.kr>
2024-08-23 22:09:59 +0900

    Docs:초기 문서 세팅

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import React from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./fonts/Pretendard.css";
+import { createGlobalStyle } from "styled-components";
+
 function App() {
   return (
     <div>
@@ -6,3 +12,19 @@ function App() {
   );
 }
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  #root{
+    max-width: 390px;
+    height: var(--app-height, 100vh);
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: "Pretendard";
+    padding: 20px 20px 0;
+    background-color: #F9F9F9;
+    box-sizing: border-box;
+  }
+`
